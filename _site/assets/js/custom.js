482eac9f1833498053f9326e554656a20ae89e22
jef210 <jmalek210@gmail.com>
2015-05-06 12:47:07 -0600

    added fixed message-bar w/ zindex

--- _site/assets/js/custom.js
+++ _site/assets/js/custom.js
@@ -6,7 +6,18 @@
 			jQuery(this).attr('rel', jQuery(this).data('gal'));
 		});  	
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
-	}); 
+    
+    // $('.nav').affix({
+    //       offset: {
+    //         top: $('top-area').height()
+    //       }
+    // }); 
+
+    // $('#sidebar').affix({
+    //       offset: {
+    //         top: 200
+    //       }
+    // });  
 
 		
 })(jQuery);
